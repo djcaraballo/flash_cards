0c916022dc4f3432c0616450a68663160a3bd3dd
Dina Caraballo <dina.caraballo@flatironschool.com>
2019-04-12 11:14:29 -0600

    create turn class - all turn tests passing

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -11,22 +11,44 @@ class TurnTest < Minitest::Test
   end
 
   def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
     assert_instance_of Turn, turn
   end
 
   def test_it_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
     assert_equal "Juneau", turn.guess
   end
 
   def test_it_has_a_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
     assert_equal card, turn.card
   end
 
   def test_it_returns_a_boolean
-    assert_equal true, card.correct?
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal true, turn.correct?
+  end
+
+  def test_it_returns_the_correct_boolean
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    assert_equal false, turn.correct?
   end
 
   def test_it_has_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
     assert_equal "Correct!", turn.feedback
+  end
+
+  def test_it_has_the_correct_feedback
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    assert_equal "Incorrect.", turn.feedback
   end
 end
